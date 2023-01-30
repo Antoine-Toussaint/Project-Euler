870dd7226e816d766f1e38d45df5ffbb6813ba2b
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2023-01-30 10:25:04 -0800

    5

--- 5.py
+++ 5.py
@@ -1,2 +1,31 @@
-def sort(l):
-    return 5
+from functools import reduce
+from operator import mul
+
+
+def union(s0, s1):
+    s = s0.copy()
+    for k1, v1 in s1.items():
+        s[k1] = max(v1, s0.get(k1, 0))
+    return s
+
+
+def prime_factors(n):
+    s = {}
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            s[i] = s.get(i, 0) + 1
+            n //= i
+        i += 1
+    if n > 1:
+        s[n] = 1
+    return s
+
+
+def solve():
+    factors = reduce(union, [prime_factors(i) for i in range(2, 21)], {})
+    return reduce(mul, [k ** v for k, v in factors.items()])
+
+
+if __name__ == "__main__":
+    print(solve())
