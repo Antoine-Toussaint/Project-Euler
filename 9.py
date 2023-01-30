5af633df5bbfb454348b3fa8623032c5386e6cb8
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2023-01-30 11:09:27 -0800

    9

---
+++ 9.py
@@ -0,0 +1,14 @@
+def is_triplet(a, b, c):
+    return a**2 + b**2 == c**2
+
+
+def solve():
+    for a in range(1, 1000):
+        for b in range(a, 1000):
+            c = 1000 - a - b
+            if is_triplet(a, b, c):
+                return a * b * c
+
+
+if __name__ == "__main__":
+    print(solve())
