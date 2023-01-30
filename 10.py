38efd000c641727cd63bb41c37de37ae477d82de
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2023-01-30 13:14:27 -0800

    10

---
+++ 10.py
@@ -0,0 +1,20 @@
+def solve():
+    primes = [2]
+    candidate = 3
+    while candidate < 2000000:
+        is_prime = True
+        sqrt = candidate ** 0.5
+        for prime in primes:
+            if prime > sqrt:
+                break
+            elif candidate % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(candidate)
+        candidate += 2
+    return sum(primes)
+
+
+if __name__ == "__main__":
+    print(solve())
