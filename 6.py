5c48bc12d88fd36f99d244000ab22b6bccf68ea5
Antoine-Toussaint <antoine.toussaint@depinfonancy.net>
2023-01-30 10:50:06 -0800

    6

---
+++ 6.py
@@ -0,0 +1,12 @@
+def sum_ind_sq(n):
+    return n * (n + 1) * (2 * n + 1) // 6
+
+
+def solve():
+    n = 100
+    s = n * (n + 1) // 2
+    return s*s - sum_ind_sq(n)
+
+
+if __name__ == "__main__":
+    print(solve())
